5b6a8803857f1bb8cf0ef6d50069c2fdee35d715
Denise Case <denisecase>
2021-04-22 17:43:12 +0000

    Initial commit

---
+++ src/main/groovy/edu/nwmissouri/dcase/Application.groovy
@@ -0,0 +1,11 @@
+package edu.nwmissouri.dcase
+
+import io.micronaut.runtime.Micronaut
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Application {
+    static void main(String[] args) {
+        Micronaut.run(Application, args)
+    }
+}
